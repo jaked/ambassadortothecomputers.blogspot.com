9786d0b1d07bc4b425900317fe96285990223eca
Jake Donham <jake@donham.org>
2010-09-03 13:50:26 -0400

    pa_let_try example for syntax extensions post

---
+++ _code/camlp4-syntax-extensions/pa_let_try/pa_let_try.ml
@@ -0,0 +1,29 @@
+open Camlp4
+
+module Id : Sig.Id =
+struct
+  let name = "pa_let_try"
+  let version = "0.1"
+end
+
+module Make (Syntax : Sig.Camlp4Syntax) =
+struct
+  open Sig
+  include Syntax
+
+  EXTEND Gram
+    expr: LEVEL "top" [
+      [ "let"; "try"; r = opt_rec; bi = binding; "in"; e = sequence; "with"; a = match_case ->
+          let a =
+            List.map
+              (function
+                 | <:match_case< $p$ when $w$ -> $e$ >> ->
+                     <:match_case< $p$ when $w$ -> fun () -> $e$ >>
+                 | mc -> mc)
+              (Ast.list_of_match_case a []) in
+          <:expr< (try let $rec:r$ $bi$ in fun () -> do { $e$ } with [ $list:a$ ])() >> ]
+    ];
+  END
+end
+
+let module M = Register.OCamlSyntaxExtension(Id)(Make) in ()
