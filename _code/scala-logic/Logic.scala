8df2f628763bba21d266da1d268634aedd7d42c3
Jake Donham <jdonham@twitter.com>
2011-05-12 21:01:40 -0700

    fair_or, fair_bind, ifte, once

--- _code/scala-logic/Logic.scala
+++ _code/scala-logic/Logic.scala
@@ -12,6 +12,38 @@ trait Logic { L =>
   def or[A](as: List[A]): T[A] =
     as.foldRight(fail[A])((a, t) => or(unit(a), t))
 
+  def fair_or[A](t1: T[A], t2: => T[A]): T[A] =
+    bind(unit(()), { _: Unit =>
+      split(t1) match {
+        case None => t2
+        case Some((a, t)) => or(unit(a), fair_or(t2, t))
+      }
+    })
+
+  def fair_bind[A,B](t: T[A], f: A => T[B]): T[B] =
+    bind(unit(()), { _: Unit =>
+      split(t) match {
+        case None => fail
+        case Some((a, t)) => fair_or(f(a), fair_bind(t, f))
+      }
+    })
+
+  def ifte[A,B](t: T[A], th: A => T[B], el: T[B]): T[B] =
+    bind(unit(()), { _: Unit =>
+      split(t) match {
+        case None => el
+        case Some((a, t)) => or(th(a), bind(t, th))
+      }
+    })
+
+  def once[A](t: T[A]): T[A] =
+    bind(unit(()), { _: Unit =>
+      split(t) match {
+        case None => fail
+        case Some((a, _)) => unit(a)
+      }
+    })
+
   def run[A](t: T[A], n: Int): List[A] = {
     def runAcc(t: T[A], n: Int, acc: List[A]): List[A] =
       if (n <= 0) acc.reverse else
@@ -22,7 +54,7 @@ trait Logic { L =>
     runAcc(t, n, Nil)
   }
 
-  case class Syntax[A](t: T[A]) {
+  class Syntax[A](t: T[A]) {
     def map[B](f: A => B): T[B] = L.apply(t, f)
     def filter(p: A => Boolean): T[A] = L.filter(t, p)
     def flatMap[B](f: A => T[B]): T[B] = L.bind(t, f)
@@ -31,5 +63,12 @@ trait Logic { L =>
     def |(t2: => T[A]): T[A] = L.or(t, t2)
   }
 
-  implicit def syntax[A](t: T[A]) = Syntax(t)
+  implicit def syntax[A](t: T[A]) = new Syntax(t)
+
+  class FairSyntax[A](t: T[A]) extends Syntax(t) {
+    override def flatMap[B](f: A => T[B]): T[B] = L.fair_bind(t, f)
+    override def |(t2: => T[A]): T[A] = L.fair_or(t, t2)
+  }
+
+  def fair[A](t: T[A]) = new FairSyntax(t)
 }
